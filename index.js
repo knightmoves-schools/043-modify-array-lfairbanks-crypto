3f39dd2fd3ee9edfc1e4396d550b15ad48562c0f
luke <lfairbanks@anamosa.k12.ia.us>
2025-12-01 19:01:03 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// Create a variable named `seasons` that contains Summer, Fall, Winter and Spring
+var seasons = ["Summer", "Fall", "Winter", "Spring"];
+
+// Update the second item in the seasons array to be Autumn
+seasons[1] = "Autumn";
+
+// Assign the innerHTML of the HTML element with the id result to the second element in the seasons array
+var resultElement = document.getElementById("result");
+if (resultElement) {
+	resultElement.innerHTML = seasons[1];
+}
 
